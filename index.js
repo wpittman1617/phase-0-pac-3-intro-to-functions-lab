575ab5ab270d086bf5823c03309e60796adb4941
William Pittman <wpittman1617@gmail.com>
2022-07-16 10:57:36 -0400

    commit tree c236dd742a008da05aaf179e3b00445d7216e953

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout (string) {
+    //to do 
+    return string.toUpperCase(); 
+}
+
+function whisper (string) {
+    //to do 
+    return string.toLowerCase(); 
+}
+
+
+function logShout(){
+    
+    const spy = "hello"
+
+    console.log (logShout(spy).toUpperCase());
+}
